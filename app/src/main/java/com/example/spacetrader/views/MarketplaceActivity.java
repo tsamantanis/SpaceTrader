cfba02606a492e3cf7804c0b3622d4b8ccf024e2
Mark Mossien <mmossien6@gatech.edu>
2019-03-04 22:34:29 -0500

    added marketplace screen

--- app/src/main/java/com/example/spacetrader/views/MarketplaceActivity.java
+++ app/src/main/java/com/example/spacetrader/views/MarketplaceActivity.java
@@ -1,7 +1,11 @@
 package com.example.spacetrader.views;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
 
 public class MarketplaceActivity extends AppCompatActivity {
+    private Spinner shipGoodsSpinner;
+    private Spinner marketGoodsSpinner;
+
 
 }
