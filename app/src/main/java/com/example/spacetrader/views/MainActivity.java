65fb6a90f84e7d602f8a18f7cc03dfbd088dacfc
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2019-03-25 21:19:30 -0400

    main activity error

--- app/src/main/java/com/example/spacetrader/views/MainActivity.java
+++ app/src/main/java/com/example/spacetrader/views/MainActivity.java
@@ -2,25 +2,8 @@ package com.example.spacetrader.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
-
-
 import com.example.spacetrader.R;
-import com.example.spacetrader.entity.Planet;
-import com.example.spacetrader.entity.Player;
-import com.example.spacetrader.entity.planets.Aegon;
-import com.example.spacetrader.entity.planets.Aldea;
-import com.example.spacetrader.entity.planets.Brax;
-import com.example.spacetrader.entity.planets.Castor;
-import com.example.spacetrader.entity.planets.Daenerys;
-import com.example.spacetrader.entity.planets.Davlos;
-import com.example.spacetrader.entity.planets.Fourmi;
-import com.example.spacetrader.entity.planets.Kafka;
-import com.example.spacetrader.entity.planets.Rhaegon;
-import com.example.spacetrader.entity.planets.Zuul;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
