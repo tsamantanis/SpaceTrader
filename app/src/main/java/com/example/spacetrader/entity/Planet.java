ec212be10053d9ab8c0b3a1a995b21e9ad8aefbc
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2019-03-25 21:07:36 -0400

    edit planet class

--- app/src/main/java/com/example/spacetrader/entity/Planet.java
+++ app/src/main/java/com/example/spacetrader/entity/Planet.java
@@ -4,13 +4,15 @@ import android.util.Log;
 
 public class Planet {
     public final String NAME;
-    public final int X_COORDINATE = 10;
-    public final int Y_COORDINATE = 130;
+    public final int X_COORDINATE;
+    public final int Y_COORDINATE;
     public int TECH_LEVEL;
     public int RESOURCE_LEVEL;
 
-    public Planet(String name) {
+    public Planet(String name, int x, int y) {
         this.NAME = name;
+        this.X_COORDINATE = x;
+        this.Y_COORDINATE = y;
         this.TECH_LEVEL = (int) (Math.random() * 8);
         this.RESOURCE_LEVEL = (int) (Math.random() * 13);
         Log.d(NAME, this.toString());
